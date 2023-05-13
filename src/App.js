ce9abb9966e419d2ff2b97414f1ad40c4eb35a36
Lalit Kumar <lalitkumar110301@gmail.com>
2023-05-13 10:59:32 +0530

    Added Home, About and Navbar components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>inotebook web app</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/about'>
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
